fc6b3c8892191dc403b80b06c19798ce9c9ad83e
reaid44 <reaidhasanmd88@gmail.com>
2026-06-26 12:46:05 +0600

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export default {
-    AI_API_KEY: process.env.AI_API_KEY,
-    RSS_URL: process.env.RSS_URL,
-    PORT: process.env.PORT || 3000
+const config = {
+  AI_API_KEY: process.env.AI_API_KEY,
+  RSS_URL: process.env.RSS_URL,
+  PORT: process.env.PORT || 3000,
 };
+
+export default config;
